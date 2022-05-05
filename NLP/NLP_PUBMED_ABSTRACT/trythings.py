9ea9735bd83ba70349d5867a2b95b44d0af989e1
marcoprenassi <83007457+marcoprenassi@users.noreply.github.com>
2022-05-05 12:46:41 +0200

    Delete trythings.py
    refuse

--- NLP/NLP_PUBMED_ABSTRACT/trythings.py
+++
@@ -1,9 +0,0 @@
-results = ["aaa", "hbbb", "cccc"]
-stringResults = 'a'
-stringResults = ''.join(res+"\n" for res in results)
-print(stringResults)
-
-import nltk
-stopwords = nltk.corpus.stopwords.words('english')
-punctuation = [',','.',';']
-print(punctuation)
